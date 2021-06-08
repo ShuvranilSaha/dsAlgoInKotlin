78cdd7378435190cdf277425cca00df78ce275c5
ShuvranilSaha <subranil.saha@gmail.com>
2021-06-08 21:00:27 +0530

    binaryTrees basic implementation

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,4 @@
+import binaryTrees.BinaryNode
 import linkedList.LinkedList
 import linkedList.Node
 import queue.ArrayListQueue
@@ -180,6 +181,22 @@ fun main() {
     } ?: println("could not find wkd blue")
 
     tree.printEachLevel()
+
+    // Binary Trees
+    val zero = BinaryNode(0)
+    val one = BinaryNode(1)
+    val five = BinaryNode(5)
+    val seven = BinaryNode(7)
+    val eight = BinaryNode(8)
+    val nine = BinaryNode(9)
+
+    seven.leftChild = one
+    one.leftChild = zero
+    one.rightChild = five
+    seven.rightChild = nine
+    nine.leftChild = eight
+
+    println(seven)
 }
 
 fun makeBeverageTree(): TreeNode<String> {
