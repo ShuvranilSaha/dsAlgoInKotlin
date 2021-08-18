254601f44f524c3254ad11836008406d0c4b0aa6
ShuvranilSaha <subranil.saha@gmail.com>
2021-08-18 20:57:05 +0530

    added trie DS implementation of remove and storeList and count

--- src/main/kotlin/trie/Main.kt
+++ src/main/kotlin/trie/Main.kt
@@ -8,4 +8,23 @@ fun main() {
             print("cute is in the trie")
         }
     }
+
+    "prefix matching" example {
+        val trie = Trie<Char>().apply {
+            insert("car")
+            insert("card")
+            insert("care")
+            insert("cared")
+            insert("cars")
+            insert("carbs")
+            insert("carapace")
+            insert("cargo")
+        }
+        println("\nCollections starting with \"car\"")
+        val prefixedWithCar = trie.collections("car")
+        println(prefixedWithCar)
+        println("\nCollections starting with \"care\"")
+        val prefixedWithCare = trie.collections("care")
+        println(prefixedWithCare)
+    }
 }
--- src/main/kotlin/trie/Trie.kt
+++ src/main/kotlin/trie/Trie.kt
@@ -12,6 +12,7 @@ class Trie<Key> {
             current = current.children[it]!!
         }
         current.isTerminating = true
+        storedList.add(list)
     }
 
     fun contains(list: List<Key>): Boolean {
@@ -22,4 +23,52 @@ class Trie<Key> {
         }
         return current.isTerminating
     }
+
+    fun remove(collection: List<Key>) {
+        var current = root
+        collection.forEach {
+            val child = current.children[it] ?: return
+            current = child
+        }
+
+        if (!current.isTerminating) return
+
+        current.isTerminating = false
+//        storedList.remove(list)
+
+        val parent = current.parent
+        while (parent != null && current.children.isEmpty() && !current.isTerminating) {
+            parent.children.remove(current.key)
+            current = parent
+        }
+    }
+
+    fun collections(prefix: List<Key>): List<List<Key>> {
+        var current = root
+        prefix.forEach {
+            val child = current.children[it] ?: return emptyList()
+            current = child
+        }
+        return collections(prefix, current)
+    }
+
+    private fun collections(prefix: List<Key>, node: TrieNode<Key>?): List<List<Key>> {
+        val results = mutableListOf<List<Key>>()
+        if (node?.isTerminating == true) {
+            results.add(prefix)
+        }
+        node?.children?.forEach { (key, node) ->
+            results.addAll(collections(prefix + key, node))
+        }
+        return results
+    }
+
+    private val storedList: MutableSet<List<Key>> = mutableSetOf()
+    private val list: List<List<Key>>
+        get() = storedList.toList()
+
+    val count: Int
+        get() = storedList.count()
+    val isEmpty: Boolean
+        get() = storedList.isEmpty()
 }
